139c66319022cda8c3ede38a8fdd78fac4ed3616
Andrew-Lodise <AL964440@wcupa.edu>
2024-01-14 21:16:44 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# First make an alpaca account
+-Go to alpaca.markets and register/sign in
+
+# Installs
+pip install alpaca-trade-api
+pip install alpaca-py *** new one ***
+
+# Update config.py 
+-Find your api keys and update them in the config file
+-save the file
+
+# Run TestBotAlpha.py
+go to TestBotAlpha.py and click run!
---
+++ beta_bot.py
@@ -0,0 +1,31 @@
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import GetAssetsRequest
+from alpaca.trading.enums import AssetClass
+
+from config import API_KEY
+from config import API_SECRET
+
+trading_client = TradingClient(API_KEY, API_SECRET, paper=True)
+
+# Get our account information.
+account = trading_client.get_account()
+
+print(f"my account: \n{account}")
+
+# Check if our account is restricted from trading.
+if account.trading_blocked:
+    print('Account is currently restricted from trading.')
+
+# Check how much money we can use to open new positions.
+print(f"${account.buying_power} is available as buying power.")
+
+# search for US equities
+search_params = GetAssetsRequest(asset_class=AssetClass.US_EQUITY)
+
+#assets = trading_client.get_all_assets(search_params)
+
+# search for AAPL
+aapl_asset = trading_client.get_asset('AAPL')
+
+if aapl_asset.tradable:
+    print('We can trade AAPL.')
